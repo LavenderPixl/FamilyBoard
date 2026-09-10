e931137f68348d0a07311c43555def5efd0f2709
Anna Jacobsen <annajacobsen1999@hotmail.com>
2026-09-10 14:26:59 +0200

    chore: user creation

--- Frontend/src/views/SignupView.vue
+++ Frontend/src/views/SignupView.vue
@@ -1,16 +1,64 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import api from "@/api.ts";
+import router from "@/router";
+
+const email = ref('');
+const username = ref('');
+const password = ref('');
+const passwordAgain = ref('');
+
+const error = ref(false);
+const errorMessage = ref('');
+
+function checkMatch() {
+  return password.value === passwordAgain.value;
+}
+
+function createUser() {
+  if (!checkMatch()) { // Check if passwords match
+    error.value = true;
+    errorMessage.value = 'Kodeordene matcher ikke.';
+    return;
+  }
+  // Check if password is within rules
+  if (!/^(?=.*[a-z])(?=.*[A-Z]).{4,}$/.test(password.value) ) {
+    error.value = true;
+    errorMessage.value = 'Kodeordet må indeholde minimum 4 karakterer,\nmed minimum en stor og en lille.';
+    return;
+  }
+  // Check if username is within rules
+  if (!/^[a-zA-Z0-9]{3,}$/.test(username.value)) {
+    error.value = true;
+    errorMessage.value = 'Brugernavn må være minimum 3 karakterer\nog kan kun indeholde bogstaver eller nummer.';
+    return;
+  }
+
+  error.value = false;
+
+  api.createUser(email.value, username.value, password.value)
+      .then(res => {
+        router.push({name: 'login', query: { created: "true"}});
+      }).catch(err => {
+        if (err.response?.status === 409) {
+          error.value = true;
+          errorMessage.value = "En bruger med denne email eksisterer allerede."
+        }
+      })
+}
 
 </script>
 
 <template>
   <main>
-    <div class="container">
+    <div class="container text-center ">
       <h1 class="text-center"> FamilyBoard </h1>
-      <form class="signup-form">
-        <input name="email" placeholder="Indtast din email" required type="email"/>
-        <input name="username" placeholder="Indtast dit brugernavn" required type="email"/>
-        <input name="password" placeholder="Indtast dit kodeord" required type="password"/>
-        <input name="password-again" placeholder="Indtast dit kodeord igen" required type="password"/>
+      <p v-if="error" class="alert-danger p-2">{{ errorMessage }}</p>
+      <form class="signup-form" @submit.prevent="createUser">
+        <input v-model="email" name="email" placeholder="Indtast din email" required type="email"/>
+        <input v-model="username" name="username" placeholder="Indtast dit brugernavn - Min. 3 karakterer" required type="text"/>
+        <input v-model="password" name="password" placeholder="Indtast dit kodeord" required type="password"/>
+        <input v-model="passwordAgain" name="passwordAgain" placeholder="Indtast dit kodeord igen" required type="password"/>
         <button type="submit">Opret bruger</button>
         <button v-on:click="$router.push('/login')" type="button"><i class="bi-arrow-left"></i> Tilbage til login</button>
       </form>
@@ -22,8 +70,14 @@
 .signup-form {
   display:flex;
   flex-direction: column;
+  width: 20rem;
+  margin: 0 auto;
 }
 .signup-form > * {
   margin-top: 1rem;
+}
+
+.alert-danger {
+  white-space: pre-line;
 }
 </style>
