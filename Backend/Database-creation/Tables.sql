bb232536764c618d5317a6c3f91b9b6dd2260293
Matkip <biegel987@gmail.com>
2026-09-10 18:43:20 +0200

    fix: update db and tables to propper name convention

--- Backend/Database-creation/Tables.sql
+++ Backend/Database-creation/Tables.sql
@@ -1,13 +1,13 @@
--- DROP DATABASE IF EXISTS familyBoard;
--- CREATE DATABASE familyBoard;
-\c familyBoard
+-- DROP DATABASE IF EXISTS family_board;
+-- CREATE DATABASE family_board;
+\c family_board
 
 CREATE TABLE families(
     id SERIAL PRIMARY KEY,
     name VARCHAR(32) 
 );
 
-CREATE TABLE familyCodes(
+CREATE TABLE family_codes(
     id SERIAL primary key,
     code CHAR(8) NOT NULL ,
     expiration date NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE tasks(
     CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE SET NULL
 );
 
-CREATE TABLE repeatingTasks(
+CREATE TABLE repeating_tasks(
     id SERIAL PRIMARY KEY,
     task VARCHAR(128) NOT NULL,
     reward INT NOT NULL,
