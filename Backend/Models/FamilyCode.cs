2f7736a8a5e242cedd6c883580de6745feb1d213
Anna Jacobsen <annajacobsen1999@hotmail.com>
2026-09-15 16:36:26 +0200

    chore: update Family class, and create FamilyCode class

---
+++ Backend/Models/FamilyCode.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Models;
+
+public class FamilyCode
+{
+    public int Id { get; set; }
+    public string Code { get; set; }
+    public DateTime Expiration { get; set; }
+    public int CreatedBy { get; set; } 
+    public int FamilyId { get; set; }
+
+    public bool IsValid()
+    {
+        if (Expiration < DateTime.UtcNow)
+        {
+            return false;
+        }
+        return true;
+    }
+}
