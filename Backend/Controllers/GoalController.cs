292baee35eefac6928372e9d281fc9ad919f4881
Anna Jacobsen <annajacobsen1999@hotmail.com>
2026-09-18 16:17:20 +0200

    feat: add goal methods, update, get, get all, get assigned and make active

--- Backend/Controllers/GoalController.cs
+++ Backend/Controllers/GoalController.cs
@@ -13,8 +13,9 @@ public class GoalController : ControllerBase
 {
     [HttpPost()]
     [Authorize]
-    public ActionResult CreateGoal(GoalDto goalDto)
+    public ActionResult<Goal> CreateGoal(GoalDto goalDto)
     {
+        if (goalDto.Cost <= 0) return BadRequest("Goal must cost more than 0");
         var userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
         User? user = UserDataAccess.GetUser(userId);
         if (user == null) return BadRequest("Cannot find user with this id");
@@ -29,7 +30,7 @@ public class GoalController : ControllerBase
 
     [HttpDelete()]
     [Authorize]
-    public ActionResult DeleteGoal(int goalId)
+    public IActionResult DeleteGoal(int goalId)
     {
         var loggedInUser = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
         User? user = UserDataAccess.GetUser(loggedInUser);
@@ -41,22 +42,77 @@ public class GoalController : ControllerBase
         return Ok();        
     }
     
+    [HttpPatch]
+    [Authorize]
+    public ActionResult<Goal> UpdateGoal(int goalId, GoalDto goalDto)
+    {
+        var loggedInUser = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        User? user = UserDataAccess.GetUser(loggedInUser);
+        if (user == null) return BadRequest("Cannot find user with this id");
+        if (!user.IsAdult) return Unauthorized("Only an adult can update goals");
+        
+        Goal? updatedGoal = GoalDataAccess.UpdateGoal(goalId, goalDto.Name, goalDto.Cost, goalDto.UserId );
+        if (updatedGoal == null) return Problem("Could not update goal.");
+        return Ok(updatedGoal);
+    }
 
-    // [HttpPatch]
-    // [Authorize]
-    // public ActionResult UpdateGoal(GoalDto goalDto)
-    // {
-    //     
-    //     
-    // }
-    // [HttpDelete]
-    // [HttpGet]
+    [HttpGet]
+    [Authorize]
+    public ActionResult<Goal> GetGoal(int goalId)
+    {
+        Goal? goal = GoalDataAccess.GetGoal(goalId);
+        if (goal == null) return BadRequest("Could not find goal with this id");
+        return Ok(goal);
+    }
+
+    public ActionResult<List<Goal>> GetGoals(int userId)
+    {
+        User? user = UserDataAccess.GetUser(userId);
+        if (user == null) return BadRequest("Could not find a user, with this id.");
+        List<Goal>? goals = GoalDataAccess.GetUserGoals(userId);
+        if (goals == null) return Ok("User has no goals associated yet.");
+        return Ok(goals);
+    }
+    // Get all goals for user
+
+    [HttpPatch("assign-goal")]
+    [Authorize]
+    public IActionResult AssignGoal(int assigneeId, int goalId)
+    {
+        var loggedInUser = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        User? user = UserDataAccess.GetUser(loggedInUser);
+        if (user == null) return BadRequest("Cannot find user with this id");
+        if (!user.IsAdult) return Unauthorized("Only an adult can assign goals");
+        
+        User? assignee = UserDataAccess.GetUser(assigneeId);
+        if (assignee == null) return BadRequest("Cannot find user with this id");
+                
+        bool assigned = GoalDataAccess.AssignGoal(assigneeId, goalId);
+        if (!assigned) return Problem("Could not assign goal.");
+        return Ok();
+    }
+
+    [HttpPatch("make-active")]
+    [Authorize]
+    public ActionResult<Goal> MakeActive(int assigneeId, int goalId)
+    {
+        User? user = UserDataAccess.GetUser(assigneeId);
+        if (user == null) return BadRequest("Could not find user with this id.");
+        
+        Goal? activeGoal = GoalDataAccess.MakeActive(assigneeId, goalId);
+        return Ok(activeGoal);
+    }
+
+
     
-    // Create
-    // Delete
-    // Make active
-    // Edit
-    // Complete
+    [HttpGet("check-if-completed")]
+    [Authorize]
+    public ActionResult<bool?> CheckIfCompleted(int goalId)
+    {
+        bool? completed = GoalDataAccess.CheckIfCompleted(goalId);
+        if (completed == null) return BadRequest("Could not find goal with that id.");
+        return Ok(completed);
+    }
     
     public class GoalDto
     {
--- Backend/DataAccess/GoalDataAccess.cs
+++ Backend/DataAccess/GoalDataAccess.cs
@@ -11,19 +11,13 @@ public static class GoalDataAccess
                                 RETURNING id, name, progress, cost, user_id";
         using var conn = Database.Database.GetConn();
 
-        Goal goal = conn.QuerySingle<Goal>(insertQuery, new { name, cost, userId});
+        Goal goal = conn.QuerySingle<Goal>(insertQuery, new { name, cost, userId });
         return goal;
     }
 
     public static bool DeleteGoal(int goalId)
     {
         string deleteQuery = @"DELETE FROM goals WHERE id = @goalId";
-        // string selectQuery = @"SELECT * FROM goals WHERE user_id = @userId";
-        // Get id of user from goal
-        // If goal belongs to user, set current goal to null 
-        
-        
-        
         using var conn = Database.Database.GetConn();
 
         try
@@ -38,12 +32,12 @@ public static class GoalDataAccess
 
         return true;
     }
-    
-    public static Goal? EditGoal(string name, int cost, int userId, int goalId)
+
+    public static Goal? UpdateGoal(int goalId, string name, int cost, int userId)
     {
         string updateQuery = "UPDATE goals SET name = @name, cost = @cost, user_id = @userId WHERE id = @goalId";
         string selectQuery = "SELECT * FROM goals WHERE id = @goalId";
-        
+
         using var conn = Database.Database.GetConn();
 
         try
@@ -64,15 +58,57 @@ public static class GoalDataAccess
     {
         string updateQuery = @"UPDATE goals  SET user_id = @userId  WHERE id = @goalId";
         using var conn = Database.Database.GetConn();
-        
-        return conn.Execute(updateQuery, new { userId, goalId }) == 1;
+
+        var updated = conn.Execute(updateQuery, new { userId, goalId });
+        if (updated == 0) return false;
+        return true;
     }
 
-    public static bool MakeActive(int userId, int goalId)
+    public static Goal? MakeActive(int userId, int goalId)
     {
-        string updateQuery = @"UPDATE users  SET current_goal = @goalId  WHERE id = @userId";
+        string checkQuery = @"SELECT user_id FROM goals WHERE id = @goalId";
+        string updateQuery = @"UPDATE users  SET current_goal_id = @goalId  WHERE id = @userId";
+        string selectQuery = @"SELECT * FROM goals WHERE id = @goalId";
         using var conn = Database.Database.GetConn();
-        
-        return conn.Execute(updateQuery, new { userId, goalId }) == 1;
+
+        int assignedUser = conn.ExecuteScalar<int>(checkQuery, new { userId });
+        if (assignedUser != userId) return null; // If current owner of goal is not assignee
+
+        var updated = conn.Execute(updateQuery, new { userId, goalId });
+        if (updated == 0) return null;
+
+        Goal updatedGoal = conn.QueryFirst<Goal>(selectQuery, new { goalId });
+
+        return updatedGoal;
+    }
+
+    public static Goal? GetGoal(int id)
+    {
+        string selectQuery = @"SELECT * FROM goals WHERE id = @id";
+        using var conn = Database.Database.GetConn();
+
+        Goal? goal = conn.QueryFirstOrDefault<Goal>(selectQuery, new { id });
+        if (goal == null) return null;
+        return goal;
+    }
+
+    public static List<Goal>? GetUserGoals(int userId)
+    {
+        string selectQuery = @"SELECT * FROM goals WHERE user_id = @userId";
+        using var conn = Database.Database.GetConn();
+
+        List<Goal>? goals = conn.Query<Goal>(selectQuery, new { userId }).ToList();
+        return goals;
+    }
+
+    public static bool? CheckIfCompleted(int goalId)
+    {
+        string selectQuery = @"SELECT * FROM goals WHERE id = @goalId";
+        using var conn = Database.Database.GetConn();
+
+        Goal? goal = conn.QueryFirstOrDefault<Goal>(selectQuery, new { goalId });
+        if (goal == null) return null;
+        if (goal.Progress < goal.Cost) return false;
+        return true;
     }
 }
