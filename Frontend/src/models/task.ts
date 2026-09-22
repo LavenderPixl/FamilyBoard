346319dfaf8a8601fcfd805cc802f7b031a06914
Matkip0 <biegel987@gmail.com>
2026-09-22 14:35:45 +0200

    feat: add expire date to task

--- Frontend/src/models/task.ts
+++ Frontend/src/models/task.ts
@@ -1,10 +1,11 @@
 export interface ITask {
-  id: number;
-  name: string;
-  reward: number;
-  completed: boolean;
-  familyId: number;
-  userId: number | null;
-  username: string | null;
-  goalId: number | null;
+  id: number
+  name: string
+  reward: number
+  completed: boolean
+  expireDate: string
+  familyId: number
+  userId: number | null
+  username: string | null
+  goalId: number | null
 }
