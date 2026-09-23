4d5b2f4494fb61488fbbe35dd954c20571b476f2
LavenderPixl <annajacobsen1999@hotmail.com>
2026-09-23 19:10:36 +0200

    feat: finish scoreboard

--- Frontend/src/views/homeViews/HasFamily.vue
+++ Frontend/src/views/homeViews/HasFamily.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 import { computed, onMounted, ref } from 'vue'
-import type { ITask } from '@/models/task.ts'
-import taskApi from '@/api/taskApi.ts'
 import { userStore } from '@/stores/userStore.ts'
 import familyApi from '@/api/familyApi.ts'
-import type { IGoal } from '@/models/goal.ts'
 import goalApi from '@/api/goalApi.ts'
+import taskApi from '@/api/taskApi.ts'
+import type { ITask } from '@/models/task.ts'
+import type { IGoal } from '@/models/goal.ts'
+import type { IUser } from "@/models/user.ts";
 
 const familyName = ref('')
 const goal = ref<IGoal>()
 const tasks = ref<ITask[]>([])
 const openTasks = ref<ITask[]>([])
 const isUserAdult = ref(false)
+const familyMembers = ref<IUser[]>([])
+
+const sortedFamilyMembers = computed(() => { // Returns new list
+  return [...familyMembers.value].sort((a,b) => b.points - a.points)
+})
 
 const goalPercentage = computed(() => {
   if (goal.value) {
@@ -21,6 +27,10 @@ const goalPercentage = computed(() => {
   return 0
 })
 
+async function refreshFamilyMembers(){
+  familyMembers.value = await familyApi.getFamilyMembers()
+}
+
 async function markTaskAsCompleted(task: ITask) {
   const updatedTask = await taskApi.markTaskAsCompleted(task)
 
@@ -29,6 +39,7 @@ async function markTaskAsCompleted(task: ITask) {
     tasks.value[index] = updatedTask
     sortTasks()
     await refreshGoal()
+    await refreshFamilyMembers()
   }
 }
 
@@ -40,6 +51,7 @@ async function unMarkTaskAsCompleted(task: ITask) {
     tasks.value[index] = updatedTask
     sortTasks()
     await refreshGoal()
+    await refreshFamilyMembers()
   }
 }
 
@@ -93,8 +105,8 @@ onMounted(async () => {
     tasks.value = await taskApi.getTasksForUser(userFromUserStore.id)
   }
   sortTasks()
-
   familyName.value = await familyApi.getFamilyName()
+  familyMembers.value = await familyApi.getFamilyMembers()
 
   await getOpenTasks()
   await refreshGoal()
@@ -126,7 +138,7 @@ onMounted(async () => {
     </div>
     <div class="row justify-content-center items-center" style="min-width: 93vw">
       <div class="col-2" id="scoreboard">
-        <h2>Rang liste // WIP</h2>
+        <h2>Rang liste</h2>
         <div class="table-scroll">
           <table class="table table-dark table-striped">
             <thead>
@@ -138,20 +150,10 @@ onMounted(async () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td>1</td>
-                <td>Søren</td>
-                <td>1230</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>sofie</td>
-                <td>1050</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>sigurd</td>
-                <td>525</td>
+              <tr v-for="(member, index) in sortedFamilyMembers" :key="member.id">
+                <td>{{ index + 1}}</td>
+                <td>{{ member.username}}</td>
+                <td>{{ member.points }}</td>
               </tr>
             </tbody>
           </table>
@@ -167,7 +169,7 @@ onMounted(async () => {
                 <th>Points</th>
                 <th>Udført</th>
                 <th>Frist</th>
-                <th>Opdater</th>
+                <th width="25%">Opdater</th>
               </tr>
             </thead>
 
